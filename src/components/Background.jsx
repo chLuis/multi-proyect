a1b05292aee435160423e3395685b20be1efc29e
Luis Chrestia <chrestia.luis@gmail.com>
2024-03-21 17:53:28 -0300

    fix bg

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -11,24 +11,27 @@ export default function Background(){
           <span className="fixed bottom-10 left-10 bg-neutral-500 rounded-full w-2 h-2"></span>
         </span>
       </div>
-        <span className="fixed top-14 left-[5%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-14 left-[60%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-12 left-[22%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-28 left-[20%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-24 left-[28%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-28 left-[66%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-20 left-[76%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-32 left-[80%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-44 left-[70%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-28 left-[40%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-32 left-[40%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-36 left-[10%] z-10 w-2 h-2 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-40 left-[32%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-44 left-[14%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-52 left-[16%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-56 left-[24%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-48 left-[28%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
-        <span className="fixed top-52 left-[44%] z-10 w-1 h-1 bg-white animate-pulse-star rounded-full"></span>
+        <span className="fixed !rotate-45 top-14 left-[5%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-14 left-[60%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-12 left-[22%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-28 left-[20%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-24 left-[28%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-28 left-[66%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-20 left-[76%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-32 left-[80%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-44 left-[70%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-28 left-[40%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-32 left-[40%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-36 left-[10%] -z-30 w-2 h-2 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-40 left-[32%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-44 left-[14%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-52 left-[16%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-56 left-[24%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-48 left-[28%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-52 left-[44%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-60 left-[44%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-64 left-[4%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
+        <span className="fixed !rotate-45 top-64 left-[74%] -z-30 w-1 h-1 bg-white animate-pulse-star"></span>
       <span className="fixed -z-10 animate-heigh-size-up bottom-52 left-0 w-[200%] h-60 bg-gradient-to-t from-blue-400 via-15% via-blue-900 to-blue-950 blur"></span>
       <span className="fixed -z-20 animate-pulse-short bottom-0 left-0 w-[200%] h-52 bg-gradient-to-t from-stone-900 to-stone-700 blur"></span>
     </>
