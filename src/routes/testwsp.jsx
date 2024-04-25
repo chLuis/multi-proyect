c083d43c006b8e3658a4bf8be425d37866442a12
Luis Chrestia <chrestia.luis@gmail.com>
2024-04-25 12:19:11 -0300

    fix input

--- src/routes/testwsp.jsx
+++ src/routes/testwsp.jsx
@@ -18,15 +18,15 @@ export default function TestWsp(){
               </div>
           </header>
           <div className='boxSearchChat'>
-            <div className='boxSearchOptions'>
+            <label name="search" className='boxSearchOptions'>
               <div className='showSearch'>
               <svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" version="1.1" x="0px" y="0px"><title>search</title><path fill="gray" d="M15.009,13.805h-0.636l-0.22-0.219c0.781-0.911,1.256-2.092,1.256-3.386 c0-2.876-2.332-5.207-5.207-5.207c-2.876,0-5.208,2.331-5.208,5.207s2.331,5.208,5.208,5.208c1.293,0,2.474-0.474,3.385-1.255 l0.221,0.22v0.635l4.004,3.999l1.194-1.195L15.009,13.805z M10.201,13.805c-1.991,0-3.605-1.614-3.605-3.605 s1.614-3.605,3.605-3.605s3.605,1.614,3.605,3.605S12.192,13.805,10.201,13.805z"></path></svg>
               </div>
               <div className='goSearch'>
               <svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" version="1.1" x="0px" y="0px"><title>back</title><path fill="rgb(1,168,132)" d="M12,4l1.4,1.4L7.8,11H20v2H7.8l5.6,5.6L12,20l-8-8L12,4z"></path></svg>
               </div>
-              <input placeholder='Buscar' type='text'/>
-            </div>
+              <input name='search' placeholder='Buscar' type='text'/>
+            </label>
             <div className='filterDontRead'>
             <svg viewBox="0 0 24 24" height="20" width="20" preserveAspectRatio="xMidYMid meet" version="1.1" x="0px" y="0px" ><title>filter</title><path fill="rgb(119,134,144)" d="M10,18.1h4v-2h-4V18.1z M3,6.1v2h18v-2H3z M6,13.1h12v-2H6V13.1z"></path></svg>
             </div>
@@ -41,7 +41,7 @@ export default function TestWsp(){
               <div className='boxChatTextPreview'>
                 <div className='boxChatText'>
                   <div className='chatText'>
-                    <p className='nameChatList'>Nombre a elegir</p>
+                    <p className='nameChatList'>Gastos</p>
                     <p className='textChatList'>Texto random que debo agregar</p>
                   </div>
                   <div className='hourShowMore'>
