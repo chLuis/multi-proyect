bf27543a402e2cb46ae4cb06f4f080c430d516e7
Luis Chrestia <chrestia.luis@gmail.com>
2024-04-29 00:35:39 -0300

    add chats

--- src/routes/testwsp.jsx
+++ src/routes/testwsp.jsx
@@ -23,12 +23,9 @@ export default function TestWsp(){
           <div className='text-center text-sm flex gap-1 items-center justify-center text-gray-500'>
           <span>
           <svg viewBox="0 0 10 12" height="14" width="10" preserveAspectRatio="xMidYMid meet" version="1.1"><title>lock-small</title><path d="M5.00847986,1.6 C6.38255462,1.6 7.50937014,2.67435859 7.5940156,4.02703389 L7.59911976,4.1906399 L7.599,5.462 L7.75719976,5.46214385 C8.34167974,5.46214385 8.81591972,5.94158383 8.81591972,6.53126381 L8.81591972,9.8834238 C8.81591972,10.4731038 8.34167974,10.9525438 7.75719976,10.9525438 L2.25767996,10.9525438 C1.67527998,10.9525438 1.2,10.4731038 1.2,9.8834238 L1.2,6.53126381 C1.2,5.94158383 1.67423998,5.46214385 2.25767996,5.46214385 L2.416,5.462 L2.41679995,4.1906399 C2.41679995,2.81636129 3.49135449,1.68973395 4.84478101,1.60510326 L5.00847986,1.6 Z M5.00847986,2.84799995 C4.31163824,2.84799995 3.73624912,3.38200845 3.6709675,4.06160439 L3.6647999,4.1906399 L3.663,5.462 L6.35,5.462 L6.35111981,4.1906399 C6.35111981,3.53817142 5.88169076,2.99180999 5.26310845,2.87228506 L5.13749818,2.85416626 L5.00847986,2.84799995 Z" fill="currentColor"></path></svg>
-
           </span>
           <span>Cifrado de extremo a extremo</span></div>
         </div>
-
-
       </div>
     
     </div>
@@ -431,32 +428,130 @@ export default function TestWsp(){
 
         <article className="boxRight ">
         <div>
-          <div className='bgFondo'></div>
-          <div className='bgChat'></div>
           <input type='radio' id='chat1' name='chat1' className='hidden input1'/>
           <div className='verChat'>
-            <div>
-              <header className='h-[59px] px-3 gap-3 w-full bg-neutral-800 text-white text-lg flex items-center'>
-              <div className='pictureChatBig'>
-                <img src='/images/wsp-chat.webp' />
-              </div>
-              <h4>
-                Gastos
-              </h4>
+            <div className='bgFondo'></div>
+            <div className='bgChat'></div>
+            <div className='absolute inset-0'>
+              <header className='h-[59px] px-3 gap-3 w-full chatHeader text-white text-lg flex items-center'>
+                <div className='pictureChatBig'>
+                  <img src='/images/wsp-chat.webp' />
+                </div>
+                <div className='chatName flex flex-col justify-center w-full'>
+                  <h4 className=''>Gastos</h4>
+                  <p className='text-xs text-neutral-400'>últ. vez hoy a la(s) 21:21</p>
+                </div>
+                <div className='flex gap-6 justify-center items-center'>
+
+                <div className='videoOptions'>
+                  <span>
+                    <svg viewBox="0 0 24 24" height="30" width="22" preserveAspectRatio="xMidYMid meet" fill="none"><title>video-call</title><path d="M3.27096 7.31042C3 7.82381 3 8.49587 3 9.84V14.16C3 15.5041 3 16.1762 3.27096 16.6896C3.5093 17.1412 3.88961 17.5083 4.35738 17.7384C4.88916 18 5.58531 18 6.9776 18H13.1097C14.502 18 15.1982 18 15.7299 17.7384C16.1977 17.5083 16.578 17.1412 16.8164 16.6896C17.0873 16.1762 17.0873 15.5041 17.0873 14.16V9.84C17.0873 8.49587 17.0873 7.82381 16.8164 7.31042C16.578 6.85883 16.1977 6.49168 15.7299 6.26158C15.1982 6 14.502 6 13.1097 6H6.9776C5.58531 6 4.88916 6 4.35738 6.26158C3.88961 6.49168 3.5093 6.85883 3.27096 7.31042Z" fill="currentColor"></path><path d="M18.7308 9.60844C18.5601 9.75994 18.4629 9.97355 18.4629 10.1974V13.8026C18.4629 14.0264 18.5601 14.2401 18.7308 14.3916L20.9567 16.3669C21.4879 16.8384 22.3462 16.4746 22.3462 15.778V8.22203C22.3462 7.52542 21.4879 7.16163 20.9567 7.63306L18.7308 9.60844Z" fill="currentColor"></path></svg>
+                  </span>
+                  <span>
+                    <svg viewBox="0 0 17 13" height="10" preserveAspectRatio="xMidYMid meet" version="1.1" x="0px" y="0px" enable-background="new 0 0 17 13"><title>chevron-down-alt</title><path fill="currentColor" d="M3.202,2.5l5.2,5.2l5.2-5.2l1.5,1.5l-6.7,6.5l-6.6-6.6L3.202,2.5z"></path></svg>
+                  </span>
+                </div>
+                <div>
+                <svg viewBox="0 0 24 24" height="30" width="30" preserveAspectRatio="xMidYMid meet" version="1.1" x="0px" y="0px"><title>search</title><path fill="rgb(134,150,160)" d="M15.009,13.805h-0.636l-0.22-0.219c0.781-0.911,1.256-2.092,1.256-3.386 c0-2.876-2.332-5.207-5.207-5.207c-2.876,0-5.208,2.331-5.208,5.207s2.331,5.208,5.208,5.208c1.293,0,2.474-0.474,3.385-1.255 l0.221,0.22v0.635l4.004,3.999l1.194-1.195L15.009,13.805z M10.201,13.805c-1.991,0-3.605-1.614-3.605-3.605 s1.614-3.605,3.605-3.605s3.605,1.614,3.605,3.605S12.192,13.805,10.201,13.805z"></path></svg>
+                </div>
+                <div>
+                <svg viewBox="0 0 24 24" height="30" width="24" preserveAspectRatio="xMidYMid meet" version="1.1" x="0px" y="0px"><title>menu</title><path fill="currentColor" d="M12,7c1.104,0,2-0.896,2-2c0-1.105-0.895-2-2-2c-1.104,0-2,0.894-2,2 C10,6.105,10.895,7,12,7z M12,9c-1.104,0-2,0.894-2,2c0,1.104,0.895,2,2,2c1.104,0,2-0.896,2-2C13.999,9.895,13.104,9,12,9z M12,15 c-1.104,0-2,0.894-2,2c0,1.104,0.895,2,2,2c1.104,0,2-0.896,2-2C13.999,15.894,13.104,15,12,15z"></path></svg>
+                </div>
+                </div>
               </header>
-              <div>
-                <div>Anotando</div>
-                <div>$20000 de luz</div>
+              <div className='chatIndividual'>
+                <div className='chatI'>
+                  <div className='colitaChatI'></div>
+                  Anotando
+                </div>
+                <div className='chatYou'>
+                  <div className='colitaChatYou'></div>
+                  $20000 de luz
+                </div>
               </div>
-              <footer className='absolute bottom-0 w-full px-3 bg-neutral-900 h-[59px] flex justify-center items-center'>
-                :D + chat Micro
+              <footer className='absolute bottom-0 w-full px-3 footerChat h-[59px] flex justify-center items-center'>
+                <span>
+                <svg viewBox="0 0 24 24" height="28" width="28" preserveAspectRatio="xMidYMid meet" version="1.1" x="0px" y="0px"><title>smiley</title><path fill="#8696a0" d="M9.153,11.603c0.795,0,1.439-0.879,1.439-1.962S9.948,7.679,9.153,7.679 S7.714,8.558,7.714,9.641S8.358,11.603,9.153,11.603z M5.949,12.965c-0.026-0.307-0.131,5.218,6.063,5.551 c6.066-0.25,6.066-5.551,6.066-5.551C12,14.381,5.949,12.965,5.949,12.965z M17.312,14.073c0,0-0.669,1.959-5.051,1.959 c-3.505,0-5.388-1.164-5.607-1.959C6.654,14.073,12.566,15.128,17.312,14.073z M11.804,1.011c-6.195,0-10.826,5.022-10.826,11.217 s4.826,10.761,11.021,10.761S23.02,18.423,23.02,12.228C23.021,6.033,17.999,1.011,11.804,1.011z M12,21.354 c-5.273,0-9.381-3.886-9.381-9.159s3.942-9.548,9.215-9.548s9.548,4.275,9.548,9.548C21.381,17.467,17.273,21.354,12,21.354z  M15.108,11.603c0.795,0,1.439-0.879,1.439-1.962s-0.644-1.962-1.439-1.962s-1.439,0.879-1.439,1.962S14.313,11.603,15.108,11.603z"></path></svg>
+                </span>
+                <span>
+                <svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" fill="none"><title>attach-menu-plus</title><path fillRule="evenodd" clipRule="evenodd" d="M20.5 13.2501L20.5 10.7501L13.25 10.7501L13.25 3.5L10.75 3.5L10.75 10.7501L3.5 10.7501L3.5 13.2501L10.75 13.2501L10.75 20.5L13.25 20.5L13.25 13.2501L20.5 13.2501Z" fill="#8696a0"></path></svg>
+                </span>
+                <input type='text' placeholder='Escribe un mensaje'/>
+                <span className='ps-4'>
+                <svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" version="1.1" x="0px" y="0px"><title>ptt</title><path fill="#8696a0" d="M11.999,14.942c2.001,0,3.531-1.53,3.531-3.531V4.35c0-2.001-1.53-3.531-3.531-3.531 S8.469,2.35,8.469,4.35v7.061C8.469,13.412,9.999,14.942,11.999,14.942z M18.237,11.412c0,3.531-2.942,6.002-6.237,6.002 s-6.237-2.471-6.237-6.002H3.761c0,4.001,3.178,7.297,7.061,7.885v3.884h2.354v-3.884c3.884-0.588,7.061-3.884,7.061-7.885 L18.237,11.412z"></path></svg>
+                </span>
               </footer>
             </div>
           </div>
         </div>
 
+        <div>
+          <input type='radio' id='chat2' name='chat1' className='hidden input1'/>
+          <div className='verChat'>
+            <div className='bgFondo'></div>
+            <div className='bgChat'></div>
+            <div className='absolute inset-0'>
+              <header className='h-[59px] px-3 gap-3 w-full chatHeader text-white text-lg flex items-center'>
+                <div className='pictureChatBig'>
+                  <img src="https://media-eze1-1.cdn.whatsapp.net/v/t61.24694-24/301439940_116500521203102_3299349136476507305_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_Q5AaIDQTMsFPzWymMU2ICewLoawZETLcNyv3skc062qNJUC6&oe=6636CB16&_nc_sid=e6ed6c&_nc_cat=108" />
+                </div>
+                <div className='chatName flex flex-col justify-center w-full'>
+                  <h4 className=''>Mariano</h4>
+                  <p className='text-xs text-neutral-400'>últ. vez hoy a la(s) 22:03</p>
+                </div>
+                <div className='flex gap-6 justify-center items-center'>
 
-
+                <div className='videoOptions'>
+                  <span>
+                    <svg viewBox="0 0 24 24" height="30" width="22" preserveAspectRatio="xMidYMid meet" fill="none"><title>video-call</title><path d="M3.27096 7.31042C3 7.82381 3 8.49587 3 9.84V14.16C3 15.5041 3 16.1762 3.27096 16.6896C3.5093 17.1412 3.88961 17.5083 4.35738 17.7384C4.88916 18 5.58531 18 6.9776 18H13.1097C14.502 18 15.1982 18 15.7299 17.7384C16.1977 17.5083 16.578 17.1412 16.8164 16.6896C17.0873 16.1762 17.0873 15.5041 17.0873 14.16V9.84C17.0873 8.49587 17.0873 7.82381 16.8164 7.31042C16.578 6.85883 16.1977 6.49168 15.7299 6.26158C15.1982 6 14.502 6 13.1097 6H6.9776C5.58531 6 4.88916 6 4.35738 6.26158C3.88961 6.49168 3.5093 6.85883 3.27096 7.31042Z" fill="currentColor"></path><path d="M18.7308 9.60844C18.5601 9.75994 18.4629 9.97355 18.4629 10.1974V13.8026C18.4629 14.0264 18.5601 14.2401 18.7308 14.3916L20.9567 16.3669C21.4879 16.8384 22.3462 16.4746 22.3462 15.778V8.22203C22.3462 7.52542 21.4879 7.16163 20.9567 7.63306L18.7308 9.60844Z" fill="currentColor"></path></svg>
+                  </span>
+                  <span>
+                    <svg viewBox="0 0 17 13" height="10" preserveAspectRatio="xMidYMid meet" version="1.1" x="0px" y="0px" enable-background="new 0 0 17 13"><title>chevron-down-alt</title><path fill="currentColor" d="M3.202,2.5l5.2,5.2l5.2-5.2l1.5,1.5l-6.7,6.5l-6.6-6.6L3.202,2.5z"></path></svg>
+                  </span>
+                </div>
+                <div>
+                <svg viewBox="0 0 24 24" height="30" width="30" preserveAspectRatio="xMidYMid meet" version="1.1" x="0px" y="0px"><title>search</title><path fill="rgb(134,150,160)" d="M15.009,13.805h-0.636l-0.22-0.219c0.781-0.911,1.256-2.092,1.256-3.386 c0-2.876-2.332-5.207-5.207-5.207c-2.876,0-5.208,2.331-5.208,5.207s2.331,5.208,5.208,5.208c1.293,0,2.474-0.474,3.385-1.255 l0.221,0.22v0.635l4.004,3.999l1.194-1.195L15.009,13.805z M10.201,13.805c-1.991,0-3.605-1.614-3.605-3.605 s1.614-3.605,3.605-3.605s3.605,1.614,3.605,3.605S12.192,13.805,10.201,13.805z"></path></svg>
+                </div>
+                <div>
+                <svg viewBox="0 0 24 24" height="30" width="24" preserveAspectRatio="xMidYMid meet" version="1.1" x="0px" y="0px"><title>menu</title><path fill="currentColor" d="M12,7c1.104,0,2-0.896,2-2c0-1.105-0.895-2-2-2c-1.104,0-2,0.894-2,2 C10,6.105,10.895,7,12,7z M12,9c-1.104,0-2,0.894-2,2c0,1.104,0.895,2,2,2c1.104,0,2-0.896,2-2C13.999,9.895,13.104,9,12,9z M12,15 c-1.104,0-2,0.894-2,2c0,1.104,0.895,2,2,2c1.104,0,2-0.896,2-2C13.999,15.894,13.104,15,12,15z"></path></svg>
+                </div>
+                </div>
+              </header>
+              <div className='chatIndividual'>
+                <div className='chatYou'>
+                  <div className='colitaChatYou'></div>
+                  Me voy a dormir
+                </div>
+                <div className='chatI'>
+                  <div className='colitaChatI'></div>
+                  Chango!
+                </div>
+                <div className='chatI'>
+                  Que hace
+                </div>
+                <div className='chatI'>
+                  Ya estas bien de la rodilla?
+                </div>
+                <div className='chatI'>
+                  Hoy se juega a la pelota, vas?
+                </div>
+              </div>
+              <footer className='absolute bottom-0 w-full px-3 footerChat h-[59px] flex justify-center items-center'>
+                <span>
+                <svg viewBox="0 0 24 24" height="28" width="28" preserveAspectRatio="xMidYMid meet" version="1.1" x="0px" y="0px"><title>smiley</title><path fill="#8696a0" d="M9.153,11.603c0.795,0,1.439-0.879,1.439-1.962S9.948,7.679,9.153,7.679 S7.714,8.558,7.714,9.641S8.358,11.603,9.153,11.603z M5.949,12.965c-0.026-0.307-0.131,5.218,6.063,5.551 c6.066-0.25,6.066-5.551,6.066-5.551C12,14.381,5.949,12.965,5.949,12.965z M17.312,14.073c0,0-0.669,1.959-5.051,1.959 c-3.505,0-5.388-1.164-5.607-1.959C6.654,14.073,12.566,15.128,17.312,14.073z M11.804,1.011c-6.195,0-10.826,5.022-10.826,11.217 s4.826,10.761,11.021,10.761S23.02,18.423,23.02,12.228C23.021,6.033,17.999,1.011,11.804,1.011z M12,21.354 c-5.273,0-9.381-3.886-9.381-9.159s3.942-9.548,9.215-9.548s9.548,4.275,9.548,9.548C21.381,17.467,17.273,21.354,12,21.354z  M15.108,11.603c0.795,0,1.439-0.879,1.439-1.962s-0.644-1.962-1.439-1.962s-1.439,0.879-1.439,1.962S14.313,11.603,15.108,11.603z"></path></svg>
+                </span>
+                <span>
+                <svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" fill="none"><title>attach-menu-plus</title><path fillRule="evenodd" clipRule="evenodd" d="M20.5 13.2501L20.5 10.7501L13.25 10.7501L13.25 3.5L10.75 3.5L10.75 10.7501L3.5 10.7501L3.5 13.2501L10.75 13.2501L10.75 20.5L13.25 20.5L13.25 13.2501L20.5 13.2501Z" fill="#8696a0"></path></svg>
+                </span>
+                <input type='text' placeholder='Escribe un mensaje'/>
+                <span className='ps-4'>
+                <svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" version="1.1" x="0px" y="0px"><title>ptt</title><path fill="#8696a0" d="M11.999,14.942c2.001,0,3.531-1.53,3.531-3.531V4.35c0-2.001-1.53-3.531-3.531-3.531 S8.469,2.35,8.469,4.35v7.061C8.469,13.412,9.999,14.942,11.999,14.942z M18.237,11.412c0,3.531-2.942,6.002-6.237,6.002 s-6.237-2.471-6.237-6.002H3.761c0,4.001,3.178,7.297,7.061,7.885v3.884h2.354v-3.884c3.884-0.588,7.061-3.884,7.061-7.885 L18.237,11.412z"></path></svg>
+                </span>
+              </footer>
+            </div>
+          </div>
+        </div>
+{/* 
         <div>
           <div className='bgFondo'></div>
           <div className='bgChat'></div>
@@ -480,7 +575,7 @@ export default function TestWsp(){
               </footer>
             </div>
           </div>
-        </div>
+        </div> */}
 
 
 
