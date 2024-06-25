e4d5ec590baee0a1187b05a70c0223c8a39ffc69
Luis Chrestia <chrestia.luis@gmail.com>
2024-06-24 23:59:33 -0300

    fix btn, add badges

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
     switch (pathname) {
       case '/':
-        title = 'Inicio';
+        title = 'Home projects';
         break;
       case '/drag-drop':
         title = 'Arrastrar y soltar';
@@ -43,12 +43,12 @@ function App() {
     <>
 
     <section className="font-cormorant flex flex-col items-center pt-32 lg:pt-20 pb-32 mx-auto animate-pulse-short bg-gray-900 duration-200">
-      <h1 className='font-bold text-center text-3xl lg:text-6xl mt-0 lg:mt-4 mb-4 animate-width text-ellipsis text-nowrap overflow-clip'>Welcome to my Projects!</h1>
+      <h1 className='font-bold text-center text-3xl lg:text-6xl mt-0 lg:mt-4 mb-4 animate-width text-ellipsis text-nowrap'>Welcome to my Projects!</h1>
       <h2 className='text-center lg:text-xl mb-0'>Explore my lates frontend projects and learn about me.</h2>
       <h4 className="text-neutral-400 text-sm">{"Developed with React(Vite) and Tailwind"}</h4>
       <div className="font-sans flex gap-6 mt-8">
-        <a href="#projects" className="font-medium outline outline-1 outline-gray-950 hover:outline-indigo-900 rounded-lg text-lg bg-gray-950 bg-opacity-20 hover:bg-opacity-90 py-3 px-4 duration-200">View Projects</a>
-        <a href="https://chluis-portfolio.netlify.app/" target="_blank" rel="noreferrer noopenner" className="font-medium text-lg rounded-lg bg-gray-950 py-3 px-4 outline outline-1 hover:bg-white hover:text-gray-950">About Me</a>
+        <a href="#projects" className="w-40 text-center font-medium outline outline-1 outline-gray-50 hover:outline-blue-600 rounded-lg text-lg bg-gray-950 bg-opacity-20 hover:text-blue-600 py-3 px-4 duration-200">View Projects</a>
+        <a href="https://chluis-portfolio.netlify.app/" target="_blank" rel="noreferrer noopenner" className="w-40 text-center font-medium text-lg rounded-lg hover:bg-gray-950 py-3 px-4 outline outline-1 bg-white text-gray-950 hover:text-white duration-200">About Me</a>
       </div>
         <Background />
     </section>
